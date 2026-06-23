a7e6ebf74ae2595db5f1f2601d713927a84ef1e3
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2026-06-23 09:49:17 +0100

    docs: add creator card swagger docs

--- core/mongoose/create-connection.js
+++ core/mongoose/create-connection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { appLogger } = require('@app-core/logger');
+
 /**
  * Mongoose Connection Config
  * @typedef {Object} MongooseConnectionConfig
@@ -17,27 +19,43 @@ const mongoose = require('mongoose');
  * @param {MongooseConnectionConfig} connectionConfig
  * @returns {Promise<MongooseConnectionResult>}
  */
-async function createConnection(connectionConfig) {
+async function createConnection(connectionConfig = {}) {
   const connectionResult = {};
   const { uri = process.env.MONGODB_URI, isNotDefault } = connectionConfig;
-  if (uri) {
-    const connectionOptions = {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-    };
-    try {
-      let connection;
-      if (isNotDefault) {
-        connection = await mongoose.createConnection(uri, connectionOptions);
-      } else {
-        ({ connection } = await mongoose.connect(uri, connectionOptions));
-      }
-      connectionResult.connection = connection;
-    } catch (e) {
-      // Todo: Proper handler?
-      throw new Error(e.message);
-    }
+
+  if (!uri) {
+    appLogger.warn({}, 'mongodb-uri-not-configured');
+    return connectionResult;
   }
+
+  const connectionOptions = {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  };
+
+  try {
+    let connection;
+
+    if (isNotDefault) {
+      connection = await mongoose.createConnection(uri, connectionOptions).asPromise();
+    } else {
+      ({ connection } = await mongoose.connect(uri, connectionOptions));
+    }
+
+    connectionResult.connection = connection;
+    appLogger.info(
+      {
+        databaseName: connection.name,
+        host: connection.host,
+        readyState: connection.readyState,
+      },
+      'mongodb-connected'
+    );
+  } catch (e) {
+    appLogger.errorX(e, 'mongodb-connection-failed');
+    throw new Error(e.message);
+  }
+
   return connectionResult;
 }
 module.exports = createConnection;
